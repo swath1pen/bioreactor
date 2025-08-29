a118d506f31b309b13ddb4baad9ea5dd79c7c0de
Swathi Penumutchu <swathipenumutchu@Swathis-MacBook-Pro-3.local>
2025-08-29 13:37:48 -0400

    debug

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -9,20 +9,20 @@ export const Home = () => {
       <RevealOnScroll>
         <div className="text-center z-10 px-4">
           <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent leading-right">
-            Hi, I'm Pedro Tech
+            Anaerobic Electrochemical Bioreactor
           </h1>
 
           <p className="tex-gray-400 text-lg mb-8 max-w-lg mx-auto">
-            I’m a full-stack developer who loves crafting clean, scalable web
-            applications. My goal is to build solutions that offer both
-            exceptional performance and a delightful user experience.
+            - 3D printable, electrochemical bioreactor
+            - customizable for 2 or 3 electrode or Hcell configurations
+            - airtight, autoclavale
           </p>
           <div className="flex justify-center space-x-4">
             <a
-              href="#projects"
+              href="#CAD Files"
               className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.4)]"
             >
-              View Projects
+              View CAD Files
             </a>
 
             <a
