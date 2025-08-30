6e8dc136cfca5ce54d18f751aaeff192bbecce54
swath1pen <swathipen@gmail.com>
2025-08-30 02:39:10 -0400

    Update LoadingScreen.jsx

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 
 export const LoadingScreen = ({ onComplete }) => {
   const [text, setText] = useState("");
-  const fullText = "<BioElectro>";
+  const fullText = "⚡Building⚡";
 
   useEffect(() => {
     let index = 0;
