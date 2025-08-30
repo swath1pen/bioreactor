ccecb82bc8fc90c48302c5771314c933c07993fa
swath1pen <swathipen@gmail.com>
2025-08-30 03:38:38 -0400

    Update Projects.jsx

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -8,12 +8,12 @@ export const Projects = () => {
     >
       <RevealOnScroll>
         <div className="max-w-5xl mx-auto px-4">
-          <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
+          <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-emerald-500 to-cyan-400 bg-clip-text text-transparent text-center">
             {" "}
             Part List
           </h2>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-            <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
+            <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-emerald-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
               <h3 className="text-xl font-bold mb-2"> Links</h3>
               <p className="text-gray-400 mb-4">
                 Additional Required Parts.
@@ -22,7 +22,7 @@ export const Projects = () => {
                 {["Glass Jar", "Gasket 1", "Gasket 2"].map((tech, key) => (
                   <span
                     key={key}
-                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
+                    className="bg-emerald-500/10 text-emerald-500 py-1 px-3 rounded-full text-sm hover:bg-emerald-500/20 
                                     hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all
                     "
                   >
@@ -34,7 +34,7 @@ export const Projects = () => {
               <div className="flex justify-between items-center">
                 <a
                   href="#"
-                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                  className="text-emerald-400 hover:text-emerald-300 transition-colors my-4"
                 >
                   McMaster →
                 </a>
@@ -43,7 +43,7 @@ export const Projects = () => {
             <div
               className="
               glass p-6 rounded-xl border border-white/10 
-              hover:-translate-y-1 hover:border-blue-500/30
+              hover:-translate-y-1 hover:border-emerald-500/30
               hover:shadow-[0_4px_20px_rgba(59,130,246,0.1)]
               transition-all
             "
@@ -57,10 +57,10 @@ export const Projects = () => {
                   <span
                     key={key}
                     className="
-                      bg-blue-500/10 text-blue-500 py-1 px-3 
+                      bg-emerald-500/10 text-emerald-500 py-1 px-3 
                       rounded-full text-sm
                       transition
-                      hover:bg-blue-500/20 hover:-translate-y-0.5
+                      hover:bg-emerald-500/20 hover:-translate-y-0.5
                       hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)]
                     "
                   >
@@ -71,7 +71,7 @@ export const Projects = () => {
               <div className="flex justify-between items-center ">
                 <a
                   href="#"
-                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                  className="text-emerald-400 hover:text-emerald-300 transition-colors my-4"
                 >
                   Purchase →
                 </a>
