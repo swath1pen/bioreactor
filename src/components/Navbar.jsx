c0950e418d905741fa7421ae927eed007236904b
swath1pen <swathipen@gmail.com>
2025-08-29 21:35:20 -0400

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import { useEffect } from "react";
+import { Link } from "react-router-dom";
 
 export const Navbar = ({ menuOpen, setMenuOpen }) => {
   useEffect(() => {
@@ -8,10 +9,9 @@ export const Navbar = ({ menuOpen, setMenuOpen }) => {
     <nav className="fixed top-0 w-full z-40 bg-[rgba(10, 10, 10, 0.8)] backdrop-blur-lg border-b border-white/10 shadow-lg">
       <div className="max-w-5xl mx-auto px-4">
         <div className="flex justify-between items-center h-16">
-          <a href="#home" className="font-mono text-xl font-bold text-white">
-            {" "}
-            BioEchem<span className="text-emerlad-300">Builds</span>{" "}
-          </a>
+          <Link to="/" className="font-mono text-xl font-bold text-white">
+            BioEchem<span className="text-emerlad-300">Builds</span>
+          </Link>
 
           <div
             className="w-7 h-5 relative cursor-pointer z-40 md:hidden"
@@ -21,34 +21,30 @@ export const Navbar = ({ menuOpen, setMenuOpen }) => {
           </div>
 
           <div className="hidden md:flex items-center space-x-8">
-            <a
-              href="#home"
-              className="text-gray-300 hove:text-white transition-colors"
+            <Link
+              to="/"
+              className="text-gray-300 hover:text-white transition-colors"
             >
-              {" "}
               Home
-            </a>
+            </Link>
             <a
               href="#about"
-              className="text-gray-300 hove:text-white transition-colors"
+              className="text-gray-300 hover:text-white transition-colors"
             >
-              {" "}
-              Usage{" "}
+              Usage
             </a>
             <a
               href="#projects"
-              className="text-gray-300 hove:text-white transition-colors"
+              className="text-gray-300 hover:text-white transition-colors"
             >
-              {" "}
-              Part List{" "}
+              Part List
             </a>
-            <a
-              href="#contact"
-              className="text-gray-300 hove:text-white transition-colors"
+            <Link
+              to="/contact"
+              className="text-gray-300 hover:text-white transition-colors"
             >
-              {" "}
-              Model{" "}
-            </a>
+              Model
+            </Link>
           </div>
         </div>
       </div>
